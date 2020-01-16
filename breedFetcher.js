f8e3c9cea55f19ef7e948668d613c90a197cf5cc
Henry Boyd <AmorphousCube@gmail.com>
2020-01-16 18:23:31 +0000

    refactor to make this tiny program into 2 microscopic fies

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,11 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], function(error, response, body) {
-  if (error) {
-    console.log('Error:', error);
-  }
-  if (body === '[]') {
-    console.log('No cat breed found with that search');
-  }
-  const data = JSON.parse(body);
-  console.log(data);
-});
+const fetchBreedDescription = function(breedname, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedname, function(error, response, body) {
+
+    const data = JSON.parse(body);
+    callback(error, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
